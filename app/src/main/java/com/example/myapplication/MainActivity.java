d091963611d1da6a9c627d31d27f817ea56caa48
smartflowers <710140919@qq.com>
2019-03-20 21:15:15 +0800

    2019.3.20

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity{
+    private Button bu_one;
+    private Button bu_two;
+    private Button bu_three;
+    private Intent i=new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_table);
+        setContentView(R.layout.activity_main);
+        bu_one=(Button)findViewById(R.id.bt1);
+        bu_two=(Button)findViewById(R.id.bt2);
+        bu_three=(Button)findViewById(R.id.bt3);
+        bu_one.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                i.setClass(com.example.myapplication.MainActivity.this,LineLayout.class);
+                startActivity(i); }
+        });
+        bu_two.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                i.setClass(com.example.myapplication.MainActivity.this,ConstraintLayout.class);
+                startActivity(i); }
+        });
+        bu_three.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                i.setClass(com.example.myapplication.MainActivity.this,TableLayout.class);
+                startActivity(i); }
+        });
     }
 }
